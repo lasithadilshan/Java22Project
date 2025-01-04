c56802757e98bc2f1c68f8077cd0e60997c316cf
Lasitha Thilakarathna <37534587+lasithadilshan@users.noreply.github.com>
2025-01-04 03:42:45 +0000

    Added Java 22 project

---
+++ HelloJava.java
@@ -0,0 +1,6 @@
+// Requires --enable-preview flag to run
+public class HelloJava {
+    void main() {
+        System.out.println("Hello, Java 22 from Codespaces!");
+    }
+}
---
+++ run.sh
@@ -0,0 +1,4 @@
+echo "Compiling..."
+javac --enable-preview --release 22 HelloJava.java
+echo "Running..."
+java --enable-preview HelloJava
